b658ef239a5cd935b35d81afd3eb7954d6ac2661
AbderrafieElmrabet <abderafieelmrabet@gmail.com>
2025-04-03 11:05:48 +0100

    2nd push

---
+++ lab-todo-compose/app/src/main/java/com/example/viewmodel/data/Task.kt
@@ -0,0 +1,12 @@
+package com.example.viewmodel.data
+
+data class Task(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val status:Int,
+    val important: Int
+)
+{
+
+}
